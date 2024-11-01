8f363fcd0b13e8f713228902e273c37e7fe4e4f5
TDVCool123 <luisandypp@gmail.com>
2024-11-01 00:09:23 -0400

    Del unecesary files

--- src/test/java/activities/contacts/ContactListScreen.java
+++
@@ -1,8 +0,0 @@
-package activities.contacts;
-
-import controls.Button;
-import org.openqa.selenium.By;
-
-public class ContactListScreen {
-    public Button addContactButton = new Button(By.id("com.android.contacts:id/floating_action_button"));
-}
